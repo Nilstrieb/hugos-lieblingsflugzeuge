933ef702c66139e6172e463fb547c7a94fe8d340
Nilstrieb <niltrieb@gmail.com>
2021-02-13 17:24:18 +0100

    spa (mit hugo)

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import {
+    Route, NavLink, HashRouter
+} from 'react-router-dom'
+import './app.css';
+import './template.css'
+import Modern from "./Militaer";
+import Home from "./Home";
+
+class App extends React.Component {
+    render() {
+        return (
+            <HashRouter>
+                <div id="parent">
+                    <header>
+                        <Navigation/>
+                        <h1>Hugos Lieblingsflugzeuge</h1>
+                    </header>
+                    <main>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/flieger-modern" component={Modern}/>
+                    </main>
+                    <Footer/>
+                </div>
+            </HashRouter>
+        );
+    }
+}
+
+export default App;
+
+class Navigation extends React.Component {
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/">Start</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/">Militärflugzeuge</NavLink>
+                        <div className="content">
+                            <NavLink to="/">Erste Weltkrieg</NavLink>
+                            <NavLink to="/">Zwischenkriegszeit</NavLink>
+                            <NavLink to="/">Zweiter Weltkrieg</NavLink>
+                            <NavLink to="/">Kalter Krieg</NavLink>
+                            <NavLink to="/flieger-modern">Moderne</NavLink>
+                        </div>
+                    </li>
+                    <li>
+                        <NavLink to="/">Airliner</NavLink>
+                        <div className="content">
+                            <NavLink to="/">Boeing</NavLink>
+                            <NavLink to="/">Airbus</NavLink>
+                            <NavLink to="/">Andere</NavLink>
+                        </div>
+                    </li>
+                    <li>
+                        <NavLink to="/">Sportflugzeuge</NavLink>
+                        <div className="content">
+                            <NavLink to="/">Doppeldecker</NavLink>
+                            <NavLink to="/">Propellerflugzeuge</NavLink>
+                            <NavLink to="/">Segelflugzeuge</NavLink>
+                            <NavLink to="/">Andere</NavLink>
+                        </div>
+                    </li>
+                    <li>
+                        <NavLink to="/">Spezielle</NavLink>
+                        <div className="content">
+                            <NavLink to="/">Helikopter</NavLink>
+                            <NavLink to="/">Zeppeline</NavLink>
+                            <NavLink to="/">Andere</NavLink>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+}
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <div>hi</div>
+            </footer>
+        )
+    }
+}
--- src/Militaer.js
+++ src/Militaer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import f22 from './img/raptor-1040097_1920.png'
 import harrier from './img/av-8b-harrier-1986422_1920.jpg'
 
-class Content extends React.Component {
+class Modern extends React.Component {
     render() {
         const items = Array(100).fill(1).map((x, y) => x + y)
             .map(x => <ModelImage img={f22} name={'F' + (20 + x) + ' Raptor'}/>);
@@ -15,9 +15,6 @@ class Content extends React.Component {
         )
     }
 }
-
-export default Content;
-
 
 class ModelImage extends React.Component {
     constructor(props) {
@@ -41,3 +38,5 @@ class ModelImage extends React.Component {
         );
     }
 }
+
+export default Modern;
